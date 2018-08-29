c4aa1fc723fa960201f7c5a33e76f2252e55bd32
avtorpc <avtorpc@gmail.com>
2018-08-29 19:37:01 +0300

    ---Fix v2.0---

--- index.php
+++ index.php
@@ -32,123 +32,125 @@ if (count($data->find('form.js-signin-form')) != 0) {
 
 //Получаем максимальный номер страницы
 $pAGE = new ClassPageResolve;
+$all_tags = [];
 
-for( $i= 1; $i<=500; $i++){
+for( $i= 1; $i<=100; $i++) {// Управляем количеством страниц которые обходим
 
-$data = str_get_html( $cURL->getPage('https://freelansim.ru/freelancers?page=' . $i ) );
+    $data = str_get_html($cURL->getPage('https://freelansim.ru/freelancers?page=' . $i));
 
-$user_arr = [];
-foreach ($data->find('.user__tags') as $key => $value) {
+    $user_arr = [];
+    foreach ($data->find('.user__tags') as $key => $value) {
 
-        if( is_null( $search_string ) ){
+        if (is_null($search_string)) {
             foreach ($data->find('.user-data__title a') as $k => $v) {
                 $link = $v->href;
-                $user_arr[md5( $link )]['link'] = $link;
+                $user_arr[md5($link)]['link'] = $link;
             }
         } else {
             foreach ($value->find('.tags__item a') as $k => $v) {
-            if( is_in_str($v->plaintext, $search_string ) ){
-                $search[] = $v->plaintext;
-                $par = $v;
-                while (true) {
-                    $par = $par->parent();
+                if (is_in_str($v->plaintext, $search_string)) {
+                    $search[] = $v->plaintext;
+                    $par = $v;
+                    while (true) {
+                        $par = $par->parent();
 
-                    if( $par->class == 'user__info') {
-                        $user_link = $par->find( '.user-data__title a' );
-                        $link = $user_link[0]->href;
+                        if ($par->class == 'user__info') {
+                            $user_link = $par->find('.user-data__title a');
+                            $link = $user_link[0]->href;
 
-                        $user_arr[md5( $link )]['link'] = $link;
-                        if( isset( $user_arr[md5( $link )]['count'] )) {
-                            $user_arr[md5( $link )]['count'] =  $user_arr[md5( $link )]['count'] + 1;
-                        } else {
-                            $user_arr[md5( $link )]['count'] = 1;
+                            $user_arr[md5($link)]['link'] = $link;
+                            if (isset($user_arr[md5($link)]['count'])) {
+                                $user_arr[md5($link)]['count'] = $user_arr[md5($link)]['count'] + 1;
+                            } else {
+                                $user_arr[md5($link)]['count'] = 1;
+                            }
+                            break;
                         }
-                        break;
-                    }
-                    if( $par->tag == 'body') {
-                        break;
+                        if ($par->tag == 'body') {
+                            break;
+                        }
                     }
                 }
             }
         }
     }
-}
 
-foreach ( $user_arr as $key_user => $value_user ){
-    $data = str_get_html($cURL->getPage('https://freelansim.ru/' . $value_user['link']));
-    $user = $data->find('.user__name_large a' );
-    $user_arr[$key_user]['name'] = $user[0]->plaintext;
-    $user = $data->find('.user-params_inline .user-params__value');
+    foreach ($user_arr as $key_user => $value_user) {
+        $data = str_get_html($cURL->getPage('https://freelansim.ru/' . $value_user['link']));
+        $user = $data->find('.user__name_large a');
+        $user_arr[$key_user]['name'] = $user[0]->plaintext;
+        $user = $data->find('.user-params_inline .user-params__value');
 
-        $user_arr[$key_user]['price'] =  isset($user[0]->plaintext) ? $user[0]->plaintext: null ;
-        $user_arr[$key_user]['experience'] = isset($user[1]->plaintext) ? $user[1]->plaintext: null ;
-        $user_arr[$key_user]['ownership'] = isset($user[2]->plaintext) ? $user[2]->plaintext: null ;
-        $user_arr[$key_user]['payment'] = isset( $user[3]->plaintext ) ? $user[3]->plaintext: null;
+        $user_arr[$key_user]['price'] = isset($user[0]->plaintext) ? $user[0]->plaintext : null;
+        $user_arr[$key_user]['experience'] = isset($user[1]->plaintext) ? $user[1]->plaintext : null;
+        $user_arr[$key_user]['ownership'] = isset($user[2]->plaintext) ? $user[2]->plaintext : null;
+        $user_arr[$key_user]['payment'] = isset($user[3]->plaintext) ? $user[3]->plaintext : null;
 
-    $user = $data->find('.user-data__about' );
-    $user_arr[$key_user]['about'] = $user[0]->plaintext;
+        $user = $data->find('.user-data__about');
+        $user_arr[$key_user]['about'] = $user[0]->plaintext;
 
-    $user_arr[$key_user]['about'] = str_replace(['Telegram', 'telegram:', 'Skype', 'skype'], [' Telegram', ' telegram:', ' Skype', ' skype'], $user_arr[$key_user]['about'] );
+        $user_arr[$key_user]['about'] = str_replace(['Telegram', 'telegram:', 'Skype', 'skype'], [' Telegram', ' telegram:', ' Skype', ' skype'], $user_arr[$key_user]['about']);
 
-    preg_match_all( "/[^@\s]++@\S++/", $user_arr[$key_user]['about'], $a);
-    if( isset( $a[0])) {
-        foreach ( $a[0] as $key=>$val) {
-            $user_arr[$key_user]['contact']['mail'][md5( $val )] = $val;
-        }
-    }
-
-    preg_match_all('/(http|https|ftp|ftps|)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/', $user_arr[$key_user]['about'], $a );
-    foreach ( $a[0] as $key=>$val) {
-        $user_arr[$key_user]['contact']['http'][md5( $val )] = $val;
-    }
-
-    preg_match_all('/Skype:(.*?\r\n)|Skype:(.*)?/is', $user_arr[$key_user]['about'], $a);
-    foreach ( $a as $key=>$val) {
-        if( isset( $val[0]) AND $val[0] != ''){
-            $user_skype = trim( str_replace(['Skype:', 'skype:', 'Skype', 'skype'], "", $val[0] ) );
-            $user_arr[$key_user]['contact']['skype'][md5( $user_skype )] = $user_skype;
+        preg_match_all("/[^@\s]++@\S++/", $user_arr[$key_user]['about'], $a);
+        if (isset($a[0])) {
+            foreach ($a[0] as $key => $val) {
+                $user_arr[$key_user]['contact']['mail'][md5($val)] = $val;
+            }
         }
 
-    }
-
-    preg_match_all('/Telegram:(.*?\r\n)|Telegram:(.*)?/is', $user_arr[$key_user]['about'], $a);
-    foreach ( $a as $key=>$val) {
-        if( isset( $val[0]) AND $val[0] != ''){
-            $user_telegram = str_replace(['Telegram:', 'telegram:', 'Telegram', 'telegram'], "", $val[0] );
-            $user_arr[$key_user]['contact']['telegramm'][md5( $user_telegram )] = $user_telegram;
+        preg_match_all('/(http|https|ftp|ftps|)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/', $user_arr[$key_user]['about'], $a);
+        foreach ($a[0] as $key => $val) {
+            $user_arr[$key_user]['contact']['http'][md5($val)] = $val;
         }
 
-    }
+        preg_match_all('/Skype:(.*?\r\n)|Skype:(.*)?/is', $user_arr[$key_user]['about'], $a);
+        foreach ($a as $key => $val) {
+            if (isset($val[0]) AND $val[0] != '') {
+                $user_skype = trim(str_replace(['Skype:', 'skype:', 'Skype', 'skype'], "", $val[0]));
+                $user_arr[$key_user]['contact']['skype'][md5($user_skype)] = $user_skype;
+            }
 
-    preg_match_all('/(8|7|\+7){0,1}[- \\\\(]{0,}([9][0-9]{2})[- \\\\)]{0,}(([0-9]{2}[- ]{0,}[0-9]{2}[- ]{0,}[0-9]{3})|([0-9]{3}[- ]{0,}[0-9]{2}[- ]{0,}[0-9]{2})|([0-9]{3}[- ]{0,}[0-9]{1}[- ]{0,}[0-9]{3})|([0-9]{2}[- ]{0,}[0-9]{3}[- ]{0,}[0-9]{2}))/', $user_arr[$key_user]['about'], $a );
-    if( isset( $a[0])){
-        foreach ( $a[0] as $key=>$val) {
-            $user_arr[$key_user]['contact']['phone'][md5( $val )] = $val;
+        }
+
+        preg_match_all('/Telegram:(.*?\r\n)|Telegram:(.*)?/is', $user_arr[$key_user]['about'], $a);
+        foreach ($a as $key => $val) {
+            if (isset($val[0]) AND $val[0] != '') {
+                $user_telegram = str_replace(['Telegram:', 'telegram:', 'Telegram', 'telegram'], "", $val[0]);
+                $user_arr[$key_user]['contact']['telegramm'][md5($user_telegram)] = $user_telegram;
+            }
+
+        }
+
+        preg_match_all('/(8|7|\+7){0,1}[- \\\\(]{0,}([9][0-9]{2})[- \\\\)]{0,}(([0-9]{2}[- ]{0,}[0-9]{2}[- ]{0,}[0-9]{3})|([0-9]{3}[- ]{0,}[0-9]{2}[- ]{0,}[0-9]{2})|([0-9]{3}[- ]{0,}[0-9]{1}[- ]{0,}[0-9]{3})|([0-9]{2}[- ]{0,}[0-9]{3}[- ]{0,}[0-9]{2}))/', $user_arr[$key_user]['about'], $a);
+        if (isset($a[0])) {
+            foreach ($a[0] as $key => $val) {
+                $user_arr[$key_user]['contact']['phone'][md5($val)] = $val;
+            }
+        }
+
+        foreach ($data->find('.user-params__value_tags a') as $key => $value) {
+            $user_arr[$key_user]['tags'][md5(trim($value->plaintext))] = $value->plaintext;
+            $all_tags[md5(trim($value->plaintext))] = trim($value->plaintext);
         }
     }
-
-    foreach( $data->find( '.user-params__value_tags a' ) as $key => $value ){
-        $user_arr[$key_user]['tags'][] = $value->plaintext;
-    }
-
-}
 
 //Записываем данные со страницы в базу
     $ins_user = '';
     $del = 'DELETE FROM lancers.users AS U WHERE U.hash IN( ';
+    $del_tags = 'DELETE FROM lancers.tags AS T WHERE T.hash IN(';
     foreach ($user_arr as $key_user => $value_user) {
         $del = $del . "'" . $key_user . "',";
         $ins_user = $ins_user . 'INSERT INTO lancers.users( hash, name_user, tag_rank, page_number, link, price, experience, ownership, payment, about, date_insert) VALUES (';
         $ins_user = $ins_user . "'" . $key_user . "',";
         $ins_user = $ins_user . "'" . pg_escape_string($value_user['name']) . "',";
-        $ins_user = $ins_user . "'" . ( isset( $value_user['count'] ) ? pg_escape_string( $value_user['count'] ): 0 ) . "',";
-        $ins_user = $ins_user . $i .",";
+        $ins_user = $ins_user . "'" . (isset($value_user['count']) ? pg_escape_string($value_user['count']) : 0) . "',";
+        $ins_user = $ins_user . $i . ",";
         $ins_user = $ins_user . "'" . pg_escape_string($value_user['link']) . "',";
         $ins_user = $ins_user . "'" . pg_escape_string($value_user['price']) . "',";
         $ins_user = $ins_user . "'" . pg_escape_string($value_user['experience']) . "',";
         $ins_user = $ins_user . "'" . pg_escape_string($value_user['ownership']) . "',";
         $ins_user = $ins_user . "'" . pg_escape_string($value_user['payment']) . "',";
-        $ins_user = $ins_user . "'" . pg_escape_string(str_replace(['\n', '\r', '\r\n'], ' ', $value_user['about'])) . "',";
+        $ins_user = $ins_user . "'" . pg_escape_string(str_replace(['\n', '\r', '\r\n'], ' ', '')) . "',";
         $ins_user = $ins_user . 'now());';
         if (isset($value_user['contact']['mail'])) {
             foreach ($value_user['contact']['mail'] as $key_phone => $val_phone) {
@@ -177,22 +179,33 @@ foreach ( $user_arr as $key_user => $value_user ){
         }
         if (isset($value_user['tags'])) {
             foreach ($value_user['tags'] as $key_tags => $val_tags) {
-                $ins_user = $ins_user . "INSERT INTO lancers.tags(tag, link_user) VALUES ( '" . $val_tags . "'," . "'" . $key_user . "');";
+                $del_tags = $del_tags . "'" . $key_tags . "',";
+                $ins_user = $ins_user . "INSERT INTO lancers.link_user_tags( user_hash, tags_hash)  VALUES ( '" . $key_user . "'," . "'" . $key_tags . "');";
             }
         }
     }
 
-  if ( count( $user_arr)>0) {
-      pg_query($conn, substr($del, 0, -1) . ')');
-      pg_query($conn, $ins_user);
-  }
+    if (count($user_arr) > 0) {
+        pg_query($conn, substr($del, 0, -1) . ')');
+        pg_query($conn, $ins_user);
+    }
+}
+
+if( count( $all_tags)>0){
+    $ins_tags = '';
+    foreach( $all_tags as $key_tags => $val_tags ) {
+        $del_tags = $del_tags . "'" . $key_tags . "',";
+        $ins_tags = $ins_tags . "INSERT INTO lancers.tags(tag, hash) VALUES ( '" . $val_tags . "'," . "'" . $key_tags . "');";
+    }
+    pg_query($conn, substr($del_tags, 0, -1) . ')');
+    pg_query($conn, $ins_tags );
 
 }
 
 function is_in_str($str,$substr)
 {
     $result = strripos ($str, $substr);
-    if ($result === FALSE) // если это действительно FALSE, а не ноль, например
+    if ($result === FALSE)
         return false;
     else
         return true;
